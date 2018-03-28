5e1b1278ef1cc1b75cadca07e17b8df42b0b7cde
Mike Korakakis <michael_1988@windowslive.com>
2018-03-28 12:54:00 -0700

    completed authentication methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:update, :show, :edit, :destroy]
+	before_action :require_user, except: [:index, :show]
+	before_action :require_same_user, only: [:edit, :update, :delete]
+
 	def index
 		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
@@ -10,7 +13,7 @@ class ArticlesController < ApplicationController
 		#render plain: params[:article]	
 			
 		@article = Article.new(article_params)
-		@article.user = User.first		
+		@article.user = current_user		
 		if @article.save
 			flash[:success] = "Article was succesfully created"
 			redirect_to article_path(@article)
@@ -48,5 +51,12 @@ private
 	end
 	def article_params
 		params.require(:article).permit(:title, :description)
+	end
+
+	def require_same_user
+		if current_user != @article.user
+			flash[:danger] = "You can only edit or delete your own articles"
+			redirect_to root_path
+		end
 	end
 
