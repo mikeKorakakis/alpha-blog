2c5589dab2f8054013534de23d33b52a72fdfc98
Mike Korakakis <michael_1988@windowslive.com>
2018-03-28 14:10:31 -0700

    try fix bug with heroku

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
 	before_action :require_same_user, only: [:edit, :update, :delete]
 
 	def index
-		@articles = Article.all#paginate(page: params[:page], per_page: 5)
+		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
 	def new
 		@article = Article.new
